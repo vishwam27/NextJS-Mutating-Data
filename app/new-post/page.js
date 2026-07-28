f7b2b9bb6e4f92da862c5f0fb1d0b8f7cc3ae0da
Vishwam patel <vishwam@Vishwams-MacBook-Air.local>
2026-07-28 17:06:02 +0530

    working:newpost button also update: feed

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,8 +1,24 @@
+import { storePost } from "@/lib/posts";
+
 export default function NewPostPage() {
+
+  async function createPost(formData){
+    "use server";
+    const title= formData.get('title');
+    const image=  formData.get('image');
+    const content=  formData.get('content');
+
+    storePost({
+      imageUrl : '',
+      title,
+      content,
+      userId: 1
+    })
+  }
   return (
     <>
       <h1>Create a new post</h1>
-      <form>
+      <form action={createPost}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
