57e8ac08a1e31bfcbb08bdbe5a11c4dd16e9fcc6
Vishwam patel <vishwam@Vishwams-MacBook-Air.local>
2026-07-28 17:53:49 +0530

    Validation: Users For Input Post

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,14 +1,29 @@
-import FormSubmit from "@/components/form-submit";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
+import PostForm from "../../components/post-form";
 
 export default function NewPostPage() {
 
-  async function createPost(formData){
+  async function createPost(prevState, formData){
     "use server";
     const title= formData.get('title');
     const image=  formData.get('image');
     const content=  formData.get('content');
+
+    let errors =[];
+    if(!title || title.trim().length === 0){
+      errors.push("Title is required.");
+    }
+    if(!content || content.trim().length === 0){
+      errors.push("Content is required.")
+    }
+    if(!image || image.size === 0){
+      errors.push("Image is required to submitted the post.")
+    }
+    if (errors.length>0){
+      return{ errors };
+    }
+    
 
     await storePost({
       imageUrl : '',
@@ -17,33 +32,10 @@ export default function NewPostPage() {
       userId: 1
     });
 
+
     redirect('/feed');
+
   }
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+ 
+  return <PostForm action={createPost} />
 }
