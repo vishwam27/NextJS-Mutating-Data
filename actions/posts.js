95f03476903bbc1dbd168c17d2970c3085292645
Vishwam patel <vishwam@Vishwams-MacBook-Air.local>
2026-07-29 11:16:15 +0530

    Bug Fix:Like Button Work Instantly

--- actions/posts.js
+++ actions/posts.js
@@ -44,6 +44,6 @@ export async function createPost(prevState, formData){
 }
 
 export async function togglePostLikeStatus(postId){
-    updatePostLikeStatus(postId, 2);
-    
+    await updatePostLikeStatus(postId, 2);
+    revalidatePath('/' , 'layout');
 }
